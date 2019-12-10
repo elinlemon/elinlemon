a811b90057d8af444cef98b5f364af930f4bb6ca
Fabian Nagel <fabian.nagel@live.de>
2019-12-10 14:33:05 +0100

    shopping cart done

--- src/ShoppingCart.js
+++ src/ShoppingCart.js
@@ -1,16 +1,21 @@
-import {
-    isEqual
-} from "lodash";
+import { isEqual } from "lodash";
 
 export class ShoppingCart {
 
     constructor() {
         this.menuItems = [];
         this.totalPrice = 0;
+
+        this.menuItemId = 0;
     }
 
     addMenuItem(menuItem) {
         menuItem.order = this.menuItems.length + 1;
+        
+        // assign an id to this menu item so that we can separate between identical ingredients among different menu items
+        menuItem.id = this.menuItemId;
+        this.menuItemId += 1;
+
         this.menuItems.push(menuItem);
         this.totalPrice += menuItem.totalPrice;
     }
@@ -53,22 +58,36 @@ export class MenuItem {
         }
     }
 
+    // returns a list of ingredients without any duplicates
+    // instead, for every ingredient a "count" attribute is added
     getPrintableIngredientList() {
-        let uniqueIngredients = _.uniqBy(this.ingredients, 'ingredient_id');
+        let countPerIngredientId = {};
 
-        for (let uq in uniqueIngredients) {
+        // determine count per ingredient
+        for (let ingredient of this.ingredients) {
+            let id = ingredient.ingredient_id;
 
-            console.log(uq);
+            let foo = countPerIngredientId[id];
 
+            // if we haven't seen this ingredient, set the count to 1
+            if (typeof(countPerIngredientId[id]) === 'undefined') {
+                countPerIngredientId[id] = 1;
+                continue;
+            }
+
+            // otherwise, increment count
+            countPerIngredientId[id] += 1;
         }
 
+        let uniqueIngredients = _.uniqBy(this.ingredients, 'ingredient_id');
 
-        return "foo";
+        // set the count as an additional field for every unique ingredient
+        for (let uq of uniqueIngredients) {
+            let id = uq.ingredient_id;
+            let count = countPerIngredientId[id];
+            uq["count"] = count;
+        }
 
-
-        /*
-        return initial ingredient object but add "count" attribute
-        */
-
+        return uniqueIngredients;
     }
 }
